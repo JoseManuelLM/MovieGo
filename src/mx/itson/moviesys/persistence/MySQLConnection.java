d426524c5f3746a6a08b1faec2b4c676afbf8734
Josem <Josem@LAPTOP-TL48B6DD>
2023-12-05 21:57:53 -0700

    Commit.

--- src/mx/itson/moviesys/persistence/MySQLConnection.java
+++ src/mx/itson/moviesys/persistence/MySQLConnection.java
@@ -4,14 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 /**
- *
- * @author Josem
+ * Proporciona un método para establecer una conexión a la base de datos MySQL.
+ * Requiere el controlador JDBC de MySQL para funcionar.
+ * 
+ * @author José Manuel Leyva Munguía
+ * @version 1.0
  */
 public class MySQLConnection {
+    /**
+     * Método estático que devuelve una conexión a la base de datos MySQL.
+     * 
+     * @return Objeto `Connection` que representa la conexión a la base de datos.
+     * @throws SQLException Si hay un problema al establecer la conexión.
+     */
     public static Connection getConnection() throws SQLException {
+        // URL de conexión a la base de datos MySQL
         String url = "jdbc:mysql://localhost:3306/proyecto_final";
+        // Credenciales de acceso a la base de datos
         String usuario = "root";
         String contraseña = "Petrolera2022!";
+        // Se establece y retorna la conexión
         return DriverManager.getConnection(url, usuario, contraseña);
     }
 }
